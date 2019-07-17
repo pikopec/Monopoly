a5a40debbbce47e775393838384fb1466bc56384
dev <name>
2019-07-17 10:48:52 +0200

    initialize classes

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 
 int main()
 {
+    Game game;
+    game.run();
     std::cout << "Hello!" << std::endl;
 }
---
+++ src/Game.cpp
@@ -0,0 +1,7 @@
+#include "Game.hpp"
+#include <iostream>
+
+void Game::run()
+{
+    std::cout << "Game started" << std::endl;
+}
